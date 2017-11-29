a6e4d227f3e029cb47e279f30ec26b9191b1c00f
Harry Hodgson <harry.hodgson@dubitlimited.com>
2017-11-29 13:06:07 +0000

    Added BlackListValidator

---
+++ Form/Validation/BlackListValidator.cs
@@ -0,0 +1,40 @@
+﻿using System.Linq;
+using DUCK.Form.Fields;
+using UnityEngine;
+
+namespace DUCK.Form.Validation
+{
+	[RequireComponent(typeof(AbstractFormField))]
+	public class BlackListValidator : AbstractValidator
+	{
+		[SerializeField]
+		private bool caseSensitive = true;
+		[SerializeField]
+		private string[] blackListedWords;
+
+		private AbstractFormField formField;
+
+		private void Awake()
+		{
+			formField = GetComponent<AbstractFormField>();
+
+			if (!caseSensitive)
+			{
+				for (var i = 0; i < blackListedWords.Length; i++)
+				{
+					blackListedWords[i] = blackListedWords[i].ToLower();
+				}
+			}
+		}
+
+		public override bool Validate()
+		{
+			var value = formField.GetStringValue();
+			if (!caseSensitive)
+			{
+				value = value.ToLower();
+			}
+			return blackListedWords.All(word => word != value);
+		}
+	}
+}
