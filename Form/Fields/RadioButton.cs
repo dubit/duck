c1bed42687a54b4b666dc8887804f48761450555
Harry Hodgson <harry.hodgson@dubitlimited.com>
2017-11-29 09:45:22 +0000

    Added Radio Buttons Field

---
+++ Form/Fields/RadioButton.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using DUCK.Utils;
+using UnityEngine;
+using UnityEngine.Experimental.UIElements;
+
+namespace DUCK.Form.Fields
+{
+	[RequireComponent(typeof(Button))]
+	public class RadioButton : MonoBehaviour
+	{
+		public string Id
+		{
+			get { return id; }
+		}
+
+		[SerializeField]
+		private string id;
+
+		public event Action OnSelected;
+
+		public virtual void SetSelected(bool selected)
+		{
+		}
+
+		protected void InvokeOnSelected()
+		{
+			OnSelected.SafeInvoke();
+		}
+	}
+}
---
+++ Form/Fields/RadioField.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using DUCK.Utils;
+using UnityEngine;
+
+namespace DUCK.Form.Fields
+{
+	public class RadioField : AbstractFormField
+	{
+		[SerializeField]
+		private RadioButton[] radioButtons;
+
+		private RadioButton selectedRadioButton;
+
+		protected override void Awake()
+		{
+			base.Awake();
+
+			if (radioButtons.Length == 0)
+			{
+				throw new Exception("Radio field needs to have atleast one radio button.");
+			}
+
+			radioButtons.ForEach(radioButton => radioButton.OnSelected += () => SelectRadio(radioButton));
+
+			SelectRadio(radioButtons[0]);
+		}
+
+		private void SelectRadio(RadioButton selected)
+		{
+			radioButtons.ForEach(radioButton => radioButton.SetSelected(radioButton == selected));
+			selectedRadioButton = selected;
+		}
+
+		public override object GetValue()
+		{
+			return selectedRadioButton.Id;
+		}
+	}
+}
