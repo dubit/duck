2e53352ee97163e05225278abc9ed710f824adc2
kkjamie <kkjamie>
2017-12-19 16:29:32 +0000

    Added warning to explain what happens when GameObject or Component references are null

--- Tween/Serialization/Editor/TweenConfigEditor.cs
+++ Tween/Serialization/Editor/TweenConfigEditor.cs
@@ -6,6 +6,7 @@ using DUCK.Serialization.Editor;
 using DUCK.Tween.Easings;
 using DUCK.Utils.Editor.EditorGUIHelpers;
 using UnityEditor;
+using UnityEngine;
 
 namespace DUCK.Tween.Serialization.Editor
 {
@@ -48,6 +49,26 @@ namespace DUCK.Tween.Serialization.Editor
 			}
 
 			ArgsListEditor.Draw(args, creatorFunction.ArgNames, customDrawFuncs);
+
+			ReportWarningForAnyNullObjectReferences(args);
+		}
+
+		private static void ReportWarningForAnyNullObjectReferences(ArgsList args)
+		{
+			for (int i = 0; i < args.AllArgs.Count; i++)
+			{
+				var type = args.ArgTypes[i];
+				if (type == typeof(GameObject) || type.IsSubclassOf(typeof(Component)))
+				{
+					if (args.AllArgs[i] == null)
+					{
+						var message = "TweenConfig contains one or more GameObject or Component references that are not set."
+							 + "\r\nWhen attempting to build the tween, the TweenBuilder will use the current GameObject or attempt to find the correct components on the current GameObject";
+						EditorGUILayout.HelpBox(message, MessageType.Info);
+						return;
+					}
+				}
+			}
 		}
 
 		private static object DrawEasingSelector(string label, object value)
