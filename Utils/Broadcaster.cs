1ed423afffc7017a6b614445c7ef1f6aaf806be9
kkjamie <kkjamie>
2018-01-08 15:20:24 +0000

    Implemented util for broadcasting events to objects

---
+++ Utils/Broadcaster.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+
+namespace DUCK.Utils
+{
+	public static class Broadcaster
+	{
+		public static void BroadcastTo<T>(GameObject gameObject, Action<T> execute, bool broadcastToChildren = true)
+		{
+			if (broadcastToChildren)
+			{
+				var children = gameObject.GetComponentsInChildren<T>();
+				foreach (var child in children)
+				{
+					execute(child);
+				}
+			}
+			else
+			{
+				var components = gameObject.GetComponents<T>();
+				foreach (var component in components)
+				{
+					execute(component);
+				}
+			}
+		}
+	}
+}
---
+++ Utils/Editor/Tests/BroadcasterTests.cs
@@ -0,0 +1,82 @@
+﻿using NUnit.Framework;
+using UnityEngine;
+
+namespace DUCK.Utils.Editor.Tests
+{
+	interface IEventHandler
+	{
+		void HandleTheEvent();
+	}
+
+	class TestBehaviour : MonoBehaviour, IEventHandler
+	{
+		public bool WasCalled { get; private set; }
+
+		public void HandleTheEvent()
+		{
+			WasCalled = true;
+		}
+	}
+
+	[TestFixture]
+	public class BroadcasterTests
+	{
+		[Test]
+		public void ExpectBroadcasterToExecuteFunctionOnTargetObject()
+		{
+			var obj = new GameObject();
+			var behaviour = obj.AddComponent<TestBehaviour>();
+
+			Broadcaster.BroadcastTo<IEventHandler>(obj, testBehaviour => testBehaviour.HandleTheEvent());
+
+			Assert.IsTrue(behaviour.WasCalled);
+
+			Object.DestroyImmediate(obj);
+		}
+
+		[Test]
+		public void ExpectBroadcasterToExecuteAllFunctionsOnTargetObject()
+		{
+			var obj = new GameObject();
+			var behaviour = obj.AddComponent<TestBehaviour>();
+			var behaviour2 = obj.AddComponent<TestBehaviour>();
+
+			Broadcaster.BroadcastTo<IEventHandler>(obj, testBehaviour => testBehaviour.HandleTheEvent());
+
+			Assert.IsTrue(behaviour.WasCalled);
+			Assert.IsTrue(behaviour2.WasCalled);
+
+			Object.DestroyImmediate(obj);
+		}
+
+		[Test]
+		public void ExpectBroadcasterToExecuteFunctionOnChildObject()
+		{
+			var obj = new GameObject();
+			var child = new GameObject();
+			child.transform.SetParent(obj.transform);
+			var behaviour = child.AddComponent<TestBehaviour>();
+
+			Broadcaster.BroadcastTo<IEventHandler>(obj, testBehaviour => testBehaviour.HandleTheEvent());
+
+			Assert.IsTrue(behaviour.WasCalled);
+
+			Object.DestroyImmediate(obj);
+		}
+
+		[Test]
+		public void ExpectBroadcasterNotToExecuteFunctionOnChildObjectIfParameterWasSet()
+		{
+			var obj = new GameObject();
+			var child = new GameObject();
+			child.transform.SetParent(obj.transform);
+			var behaviour = child.AddComponent<TestBehaviour>();
+
+			Broadcaster.BroadcastTo<IEventHandler>(obj, testBehaviour => testBehaviour.HandleTheEvent(), false);
+
+			Assert.IsFalse(behaviour.WasCalled);
+
+			Object.DestroyImmediate(obj);
+		}
+	}
+}
