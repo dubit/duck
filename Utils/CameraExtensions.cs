d1fa8b71d768e6293f2fc9004f6f117d42a8e077
DUBITLIMITED\david.smithson <david.smithson@dubitlimited.com>
2018-01-26 15:58:06 +0000

    CameraExtensions util with TakeScreenshot function

---
+++ Utils/CameraExtensions.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace DUCK.Utils
+{
+	public static class CameraExtensions
+	{
+		public static Texture2D TakeScreenshot(this Camera camera, int width, int height, TextureFormat textureFormat = TextureFormat.ARGB32)
+		{
+			var renderTexture = new RenderTexture(width, height, 24);
+			var screenShot = new Texture2D(width, height, textureFormat, false);
+
+			camera.targetTexture = renderTexture;
+			camera.Render();
+			camera.targetTexture = null;
+
+			RenderTexture.active = renderTexture;
+			screenShot.ReadPixels(new Rect(0, 0, width, height), 0, 0);
+			screenShot.Apply();
+			RenderTexture.active = null;
+
+			renderTexture.Release();
+
+			return screenShot;
+		}
+	}
+}
