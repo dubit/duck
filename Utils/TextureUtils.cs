b7a01cdd25f5d40cc5a805c1f0acbe8b5c5cfddd
DUBITLIMITED\david.smithson <david.smithson@dubitlimited.com>
2018-01-29 17:26:01 +0000

    Params added

--- Utils/TextureUtils.cs
+++ Utils/TextureUtils.cs
@@ -7,6 +7,12 @@ namespace DUCK.Utils
 		/// <summary>
 		/// Combine two textures by writing the contents of one on top of the other, at a specified offset
 		/// </summary>
+		/// <param name="baseTexture">The texture onto which to write a second texture.</param>
+		/// <param name="addedTexture">The texture to be written onto baseTexture.</param>
+		/// <param name="xOffset">X offset (from bottom-left) of the added texture sub-area.</param>
+		/// <param name="yOffset">Y offset (from bottom-left, upwards) of the added texture sub-area.</param>
+		/// <param name="additive">Whether to add the textures (true) or draw the addition over the base texture (false)</param>
+		/// <returns>The newly created Texture2D.</returns>
 		public static Texture2D CombineTextures(Texture2D baseTexture, Texture2D addedTexture, int xOffset = 0, int yOffset = 0, bool additive = false)
 		{
 			// Get the pixel (flattened) arrays of the base and overlay textures
