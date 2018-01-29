69421c6dad3bb1315746fdb60ad18436e4ca7c9d
DUBITLIMITED\david.smithson <david.smithson@dubitlimited.com>
2018-01-29 17:21:23 +0000

    Simple comments added

--- Utils/TextureUtils.cs
+++ Utils/TextureUtils.cs
@@ -4,19 +4,26 @@ namespace DUCK.Utils
 {
 	public static class TextureUtils
 	{
+		/// <summary>
+		/// Combine two textures by writing the contents of one on top of the other, at a specified offset
+		/// </summary>
 		public static Texture2D CombineTextures(Texture2D baseTexture, Texture2D addedTexture, int xOffset = 0, int yOffset = 0, bool additive = false)
 		{
+			// Get the pixel (flattened) arrays of the base and overlay textures
 			var basePixels = baseTexture.GetPixels();
 			var addedPixels = addedTexture.GetPixels();
 
 			for (var p = 0; p < basePixels.Length; p++)
 			{
+				// Convert linear index to [x,y] position on baseTexture
 				var tx = p % baseTexture.width;
 				var ty = (p - tx) / baseTexture.width;
 
+				// Transform the [x,y] into the space of the overlay texture by subtracting the offset
 				tx -= xOffset;
 				ty -= yOffset;
 
+				// If these coordinates fall within the overlay, add its pixels to the base
 				if ((tx >= 0 && tx < addedTexture.width) && (ty >= 0 && ty < addedTexture.height))
 				{
 					var inputColor = addedPixels[ty * addedTexture.width + tx];
@@ -28,6 +35,7 @@ namespace DUCK.Utils
 				}
 			}
 
+			// Save the modified pixel array as a new texture
 			var texture = new Texture2D(baseTexture.width, baseTexture.height, TextureFormat.ARGB32, false);
 			texture.SetPixels(basePixels);
 			texture.Apply();
