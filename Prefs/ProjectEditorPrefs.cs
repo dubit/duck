3fab48706cbe92664c12a18f6a6f4865b0a35ebb
DUBITLIMITED\david.smithson <david.smithson@dubitlimited.com>
2018-02-15 11:01:14 +0000

    Fix for build failure bug with Editor namespace

--- Prefs/ProjectEditorPrefs.cs
+++ Prefs/ProjectEditorPrefs.cs
@@ -1,9 +1,9 @@
-﻿using System;
+﻿#if UNITY_EDITOR
+using System;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 
-#if UNITY_EDITOR
 namespace DUCK.Prefs
 {
 	public static class ProjectEditorPrefs
