5a17a88423f9bce2eca03d3eb7e418154590f388
DUBITLIMITED\david.smithson <david.smithson@dubitlimited.com>
2018-05-09 09:47:09 +0100

    Timer.SafeStop() extension added

---
+++ Utils/TimerExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+
+namespace DUCK.Utils
+{
+	public static class TimerExtensions
+	{
+		/// <summary>
+		/// Stops a timer if the reference is valid and it is running.
+		/// </summary>
+		/// <param name="timer">The target Timer</param>
+		public static void SafeStop(this Timer timer)
+		{
+			if (timer != null && timer.IsRunning)
+			{
+				timer.Stop();
+			}
+		}
+	}
+}
