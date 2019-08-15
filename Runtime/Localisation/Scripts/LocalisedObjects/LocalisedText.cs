abc2908cfb8777654a3052a47d67cb6c1b52614b
Dan Hall <daniel.hall@dubitlimited.com>
2019-08-15 16:17:25 +0100

    Removed the require component from localised text

--- Runtime/Localisation/Scripts/LocalisedObjects/LocalisedText.cs
+++ Runtime/Localisation/Scripts/LocalisedObjects/LocalisedText.cs
@@ -1,12 +1,10 @@
-﻿using UnityEngine;
-using UnityEngine.UI;
+﻿using UnityEngine.UI;
 
 namespace DUCK.Localisation.LocalisedObjects
 {
 	/// <summary>
 	/// Automatically updates a Text component's text to the current locale
 	/// </summary>
-	[RequireComponent(typeof(Text))]
 	public class LocalisedText : AbstractLocalisedObject<Text>
 	{
 		protected override void HandleLocaleChanged(bool translationFound, string localisedString)
